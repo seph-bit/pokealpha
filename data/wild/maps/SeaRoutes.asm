80b69a7156d399feaa3027b4dd9ef10f9af867fd
seph-bit <seraphim.beatle@gmail.com>
2026-03-06 12:00:45 -0600

    Update SeaRoutes.asm

--- data/wild/maps/SeaRoutes.asm
+++ data/wild/maps/SeaRoutes.asm
@@ -3,14 +3,14 @@ SeaRoutesWildMons:
 	end_grass_wildmons
 
 	def_water_wildmons 5 ; encounter rate
-	db  5, TENTACOOL
-	db 10, TENTACOOL
-	db 15, TENTACOOL
-	db  5, TENTACOOL
-	db 10, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACOOL
 	db 30, TENTACOOL
+	db 30, MAGIKARP
+	db 37, TENTACRUEL
 	db 35, TENTACOOL
+	db 39, GYARADOS
 	db 40, TENTACOOL
+	db 40, TENTACRUEL
+	db 42, GYARADOS
+	db 40, LAPRAS
+	db 43, LAPRAS
 	end_water_wildmons
